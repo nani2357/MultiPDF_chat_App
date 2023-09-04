53b848d8c2bd301489806b776ed4bd56512fd773
naveen_kadampally <naveenkadampally@gmail.com>
2023-09-04 21:38:08 +0100

    requirements_added

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title = 'Chat with multi pdfs', page_icon = ':books')
+    
+    st.header('Chat with multiple PDFs :books:')
+    
+    print('hello world')
+
+
+
+if __name__ == 'main':
+    main()
